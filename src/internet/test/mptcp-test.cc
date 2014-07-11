d6a81ff133c7c5cdaa5d368fab2490863ac0ad44
Matthieu Coudron <mattator@gmail.com>
2014-07-11 17:55:01 +0200

    Now the test compiles with MPTCP socket factory

--- src/internet/test/mptcp-test.cc
+++ src/internet/test/mptcp-test.cc
@@ -53,7 +53,7 @@
 #include "ns3/internet-stack-helper.h"
 #include "ns3/applications-module.h"
 #include "ns3/network-module.h"
-//#include "ns3/mp-tcp-socket-factory.h"
+#include "ns3/mp-tcp-socket-factory-impl.h"
 #include "ns3/internet-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/network-module.h"
@@ -83,18 +83,23 @@ class MpTcpTestCase : public TestCase
 public:
   MpTcpTestCase(std::string name);
   virtual ~MpTcpTestCase ();
-  virtual void DoRun(void);
+
+protected:
+    void
+  SetupDefaultSim (void);
+private:
+//  virtual void DoSetup(void) = 0;
+  virtual void DoRun(void) = 0;
   virtual void DoTeardown (void);
 
-private:
+
   void ServerHandleConnectionCreated (Ptr<Socket> s, const Address & addr);
   void ServerHandleRecv (Ptr<Socket> sock);
   void ServerHandleSend (Ptr<Socket> sock, uint32_t available);
   void SourceHandleSend (Ptr<Socket> sock, uint32_t available);
   void SourceHandleRecv (Ptr<Socket> sock);
 
-  void
-  SetupDefaultSim (void);
+
 
   Ptr<Node> m_client;
   Ptr<Node> m_server;
@@ -104,17 +109,17 @@ class MpTcpAddressTestCase : public MpTcpTestCase
 {
 public:
   MpTcpAddressTestCase();
-  virtual ~MpTcpAddressTestCase ();
+  virtual ~MpTcpAddressTestCase () {}
 
-//  virtual void DoRun(void);
 
 private:
+  virtual void DoRun(void);
 
 };
 
 MpTcpTestCase::MpTcpTestCase(std::string name) : TestCase(name)
 {
-  NS_LOG_LOGIC ("For me (interface broadcast address)");
+  NS_LOG_LOGIC (this);
 
 }
 
@@ -126,13 +131,13 @@ MpTcpTestCase::~MpTcpTestCase()
 
 MpTcpAddressTestCase::MpTcpAddressTestCase() : MpTcpTestCase("Mptcp address handling")
 {
-
+  NS_LOG_LOGIC (this);
 }
 
-MpTcpAddressTestCase::~MpTcpAddressTestCase()
-{
-
-}
+//MpTcpAddressTestCase::~MpTcpAddressTestCase()
+//{
+//
+//}
 
 
 void
@@ -141,17 +146,26 @@ MpTcpTestCase::DoTeardown (void)
   Simulator::Destroy();
 }
 
-void
-MpTcpTestCase::DoRun (void)
-{
 
+//void
+//MpTcpAddressTestCase::DoRun (void)
+//{
+//  NS_LOG_LOGIC ("Doing run MpTcpAddressTestCase");
+//  MpTcpTestCase::DoRun ();
+//}
+
+
+void
+MpTcpAddressTestCase::DoRun (void)
+{
+  NS_LOG_LOGIC ("Doing run in MpTcpAddressTestCase");
   SetupDefaultSim();
   Simulator::Run();
 
   NS_LOG_LOGIC("Simulation ended");
 
-//  NS_TEST_EXPECT_MSG_EQ (0, 1, "This test must fail (not ready)");
-
+  NS_TEST_EXPECT_MSG_EQ (0, 1, "This test must fail (not ready)");
+  NS_TEST_ASSERT_MSG_EQ (true, true, "true doesn't equal true for some reason");
 }
 
 //MpTcpAddressTestCase
@@ -199,11 +213,15 @@ ns3::PacketMetadata::Enable ();
 //  m_server->
 //  m_client->Connect( serverremoteaddr );
 // lSocket
-  Ptr<SocketFactory> sockFactory0 = m_server->GetObject<MpTcpSocketFactory> ();
-  Ptr<SocketFactory> sockFactory1 = m_client->GetObject<MpTcpSocketFactory> ();
+  
+  Ptr<MpTcpSocketFactoryImpl> sockFactory0 = CreateObject<MpTcpSocketFactoryImpl> ();
+  sockFactory0->SetTcp(m_server->GetObject<TcpL4Protocol> ());
+  
+  m_server->AggregateObject(sockFactory0);
+//  Ptr<SocketFactory> sockFactory1 = m_client->GetObject<MpTcpSocketFactory> ();
   
   Ptr<Socket> serverSock = sockFactory0->CreateSocket ();
-  Ptr<Socket> clientSock = sockFactory1->CreateSocket ();
+  Ptr<Socket> clientSock = sockFactory0->CreateSocket ();
 
   serverSock->Bind(serverLocalAddr);
   serverSock->Listen();
@@ -256,6 +274,8 @@ MpTcpTestCase::SourceHandleSend (Ptr<Socket> sock, uint32_t available)
   NS_LOG_INFO("Src handle send ");
 }
 
+
+//  virtual void DoSetup (void);
 void
 MpTcpTestCase::SourceHandleRecv (Ptr<Socket> sock)
 {
@@ -285,381 +305,4 @@ public:
   }
 
 } g_MpTcpTestSuite;
-
-
-#if 0
-class MpTcpTestCase : public TestCase
-{
-public:
-  MpTcpTestCase (uint32_t totalStreamSize,
-               uint32_t sourceWriteSize,
-               uint32_t sourceReadSize,
-               uint32_t serverWriteSize,
-               uint32_t serverReadSize,
-               bool useIpv6);
-private:
-  virtual void DoRun (void);
-  virtual void DoTeardown (void);
-  void SetupDefaultSim (void);
-  void SetupDefaultSim6 (void);
-
-  Ptr<Node> CreateInternetNode (void);
-  Ptr<Node> CreateInternetNode6 (void);
-  Ptr<SimpleNetDevice> AddSimpleNetDevice (Ptr<Node> node, const char* ipaddr, const char* netmask);
-  Ptr<SimpleNetDevice> AddSimpleNetDevice6 (Ptr<Node> node, Ipv6Address ipaddr, Ipv6Prefix prefix);
-  void ServerHandleConnectionCreated (Ptr<Socket> s, const Address & addr);
-  void ServerHandleRecv (Ptr<Socket> sock);
-  void ServerHandleSend (Ptr<Socket> sock, uint32_t available);
-  void SourceHandleSend (Ptr<Socket> sock, uint32_t available);
-  void SourceHandleRecv (Ptr<Socket> sock);
-
-  uint32_t m_totalBytes;
-  uint32_t m_sourceWriteSize;
-  uint32_t m_sourceReadSize;
-  uint32_t m_serverWriteSize;
-  uint32_t m_serverReadSize;
-  uint32_t m_currentSourceTxBytes;
-  uint32_t m_currentSourceRxBytes;
-  uint32_t m_currentServerRxBytes;
-  uint32_t m_currentServerTxBytes;
-  uint8_t *m_sourceTxPayload;
-  uint8_t *m_sourceRxPayload;
-  uint8_t* m_serverRxPayload;
-
-  bool m_useIpv6;
-};
-
-static std::string Name (std::string str, uint32_t totalStreamSize,
-                         uint32_t sourceWriteSize,
-                         uint32_t serverReadSize,
-                         uint32_t serverWriteSize,
-                         uint32_t sourceReadSize,
-                         bool useIpv6)
-{
-  std::ostringstream oss;
-  oss << str << " total=" << totalStreamSize << " sourceWrite=" << sourceWriteSize
-      << " sourceRead=" << sourceReadSize << " serverRead=" << serverReadSize
-      << " serverWrite=" << serverWriteSize << " useIpv6=" << useIpv6;
-  return oss.str ();
-}
-
-#ifdef NS3_LOG_ENABLE
-static std::string GetString (Ptr<Packet> p)
-{
-  std::ostringstream oss;
-  p->CopyData (&oss, p->GetSize ());
-  return oss.str ();
-}
-#endif /* NS3_LOG_ENABLE */
-
-MpTcpTestCase::MpTcpTestCase (uint32_t totalStreamSize,
-                          uint32_t sourceWriteSize,
-                          uint32_t sourceReadSize,
-                          uint32_t serverWriteSize,
-                          uint32_t serverReadSize,
-                          bool useIpv6)
-  : TestCase (Name ("Send string data from client to server and back",
-                    totalStreamSize,
-                    sourceWriteSize,
-                    serverReadSize,
-                    serverWriteSize,
-                    sourceReadSize,
-                    useIpv6)),
-    m_totalBytes (totalStreamSize),
-    m_sourceWriteSize (sourceWriteSize),
-    m_sourceReadSize (sourceReadSize),
-    m_serverWriteSize (serverWriteSize),
-    m_serverReadSize (serverReadSize),
-    m_useIpv6 (useIpv6)
-{
-}
-
-void
-MpTcpTestCase::DoRun (void)
-{
-  m_currentSourceTxBytes = 0;
-  m_currentSourceRxBytes = 0;
-  m_currentServerRxBytes = 0;
-  m_currentServerTxBytes = 0;
-  m_sourceTxPayload = new uint8_t [m_totalBytes];
-  m_sourceRxPayload = new uint8_t [m_totalBytes];
-  m_serverRxPayload = new uint8_t [m_totalBytes];
-  for(uint32_t i = 0; i < m_totalBytes; ++i)
-    {
-      uint8_t m = (uint8_t)(97 + (i % 26));
-      m_sourceTxPayload[i] = m;
-    }
-  memset (m_sourceRxPayload, 0, m_totalBytes);
-  memset (m_serverRxPayload, 0, m_totalBytes);
-
-  if (m_useIpv6 == true)
-    {
-      SetupDefaultSim6 ();
-    }
-  else
-    {
-      SetupDefaultSim ();
-    }
-
-  Simulator::Run ();
-
-  NS_TEST_EXPECT_MSG_EQ (m_currentSourceTxBytes, m_totalBytes, "Source sent all bytes");
-  NS_TEST_EXPECT_MSG_EQ (m_currentServerRxBytes, m_totalBytes, "Server received all bytes");
-  NS_TEST_EXPECT_MSG_EQ (m_currentSourceRxBytes, m_totalBytes, "Source received all bytes");
-  NS_TEST_EXPECT_MSG_EQ (memcmp (m_sourceTxPayload, m_serverRxPayload, m_totalBytes), 0,
-                         "Server received expected data buffers");
-  NS_TEST_EXPECT_MSG_EQ (memcmp (m_sourceTxPayload, m_sourceRxPayload, m_totalBytes), 0,
-                         "Source received back expected data buffers");
-}
-void
-MpTcpTestCase::DoTeardown (void)
-{
-  delete [] m_sourceTxPayload;
-  delete [] m_sourceRxPayload;
-  delete [] m_serverRxPayload;
-  Simulator::Destroy ();
-}
-
-void
-MpTcpTestCase::ServerHandleConnectionCreated (Ptr<Socket> s, const Address & addr)
-{
-  s->SetRecvCallback (MakeCallback (&MpTcpTestCase::ServerHandleRecv, this));
-  s->SetSendCallback (MakeCallback (&MpTcpTestCase::ServerHandleSend, this));
-}
-
-void
-MpTcpTestCase::ServerHandleRecv (Ptr<Socket> sock)
-{
-  while (sock->GetRxAvailable () > 0)
-    {
-      uint32_t toRead = std::min (m_serverReadSize, sock->GetRxAvailable ());
-      Ptr<Packet> p = sock->Recv (toRead, 0);
-      if (p == 0 && sock->GetErrno () != Socket::ERROR_NOTERROR)
-        {
-          NS_FATAL_ERROR ("Server could not read stream at byte " << m_currentServerRxBytes);
-        }
-      NS_TEST_EXPECT_MSG_EQ ((m_currentServerRxBytes + p->GetSize () <= m_totalBytes), true,
-                             "Server received too many bytes");
-      NS_LOG_DEBUG ("Server recv data=\"" << GetString (p) << "\"");
-      p->CopyData (&m_serverRxPayload[m_currentServerRxBytes], p->GetSize ());
-      m_currentServerRxBytes += p->GetSize ();
-      ServerHandleSend (sock, sock->GetTxAvailable ());
-    }
-}
-
-void
-MpTcpTestCase::ServerHandleSend (Ptr<Socket> sock, uint32_t available)
-{
-  while (sock->GetTxAvailable () > 0 && m_currentServerTxBytes < m_currentServerRxBytes)
-    {
-      uint32_t left = m_currentServerRxBytes - m_currentServerTxBytes;
-      uint32_t toSend = std::min (left, sock->GetTxAvailable ());
-      toSend = std::min (toSend, m_serverWriteSize);
-      Ptr<Packet> p = Create<Packet> (&m_serverRxPayload[m_currentServerTxBytes], toSend);
-      NS_LOG_DEBUG ("Server send data=\"" << GetString (p) << "\"");
-      int sent = sock->Send (p);
-      NS_TEST_EXPECT_MSG_EQ ((sent != -1), true, "Server error during send ?");
-      m_currentServerTxBytes += sent;
-    }
-  if (m_currentServerTxBytes == m_totalBytes)
-    {
-      sock->Close ();
-    }
-}
-
-void
-MpTcpTestCase::SourceHandleSend (Ptr<Socket> sock, uint32_t available)
-{
-  while (sock->GetTxAvailable () > 0 && m_currentSourceTxBytes < m_totalBytes)
-    {
-      uint32_t left = m_totalBytes - m_currentSourceTxBytes;
-      uint32_t toSend = std::min (left, sock->GetTxAvailable ());
-      toSend = std::min (toSend, m_sourceWriteSize);
-      Ptr<Packet> p = Create<Packet> (&m_sourceTxPayload[m_currentSourceTxBytes], toSend);
-      NS_LOG_DEBUG ("Source send data=\"" << GetString (p) << "\"");
-      int sent = sock->Send (p);
-      NS_TEST_EXPECT_MSG_EQ ((sent != -1), true, "Error during send ?");
-      m_currentSourceTxBytes += sent;
-    }
-}
-
-void
-MpTcpTestCase::SourceHandleRecv (Ptr<Socket> sock)
-{
-  while (sock->GetRxAvailable () > 0 && m_currentSourceRxBytes < m_totalBytes)
-    {
-      uint32_t toRead = std::min (m_sourceReadSize, sock->GetRxAvailable ());
-      Ptr<Packet> p = sock->Recv (toRead, 0);
-      if (p == 0 && sock->GetErrno () != Socket::ERROR_NOTERROR)
-        {
-          NS_FATAL_ERROR ("Source could not read stream at byte " << m_currentSourceRxBytes);
-        }
-      NS_TEST_EXPECT_MSG_EQ ((m_currentSourceRxBytes + p->GetSize () <= m_totalBytes), true,
-                             "Source received too many bytes");
-      NS_LOG_DEBUG ("Source recv data=\"" << GetString (p) << "\"");
-      p->CopyData (&m_sourceRxPayload[m_currentSourceRxBytes], p->GetSize ());
-      m_currentSourceRxBytes += p->GetSize ();
-    }
-  if (m_currentSourceRxBytes == m_totalBytes)
-    {
-      sock->Close ();
-    }
-}
-
-Ptr<Node>
-MpTcpTestCase::CreateInternetNode ()
-{
-  Ptr<Node> node = CreateObject<Node> ();
-  //ARP
-  Ptr<ArpL3Protocol> arp = CreateObject<ArpL3Protocol> ();
-  node->AggregateObject (arp);
-  //IPV4
-  Ptr<Ipv4L3Protocol> ipv4 = CreateObject<Ipv4L3Protocol> ();
-  //Routing for Ipv4
-  Ptr<Ipv4ListRouting> ipv4Routing = CreateObject<Ipv4ListRouting> ();
-  ipv4->SetRoutingProtocol (ipv4Routing);
-  Ptr<Ipv4StaticRouting> ipv4staticRouting = CreateObject<Ipv4StaticRouting> ();
-  ipv4Routing->AddRoutingProtocol (ipv4staticRouting, 0);
-  node->AggregateObject (ipv4);
-  //ICMP
-  Ptr<Icmpv4L4Protocol> icmp = CreateObject<Icmpv4L4Protocol> ();
-  node->AggregateObject (icmp);
-  //UDP
-  Ptr<UdpL4Protocol> udp = CreateObject<UdpL4Protocol> ();
-  node->AggregateObject (udp);
-  //TCP
-  Ptr<TcpL4Protocol> tcp = CreateObject<TcpL4Protocol> ();
-  node->AggregateObject (tcp);
-  return node;
-}
-
-Ptr<SimpleNetDevice>
-MpTcpTestCase::AddSimpleNetDevice (Ptr<Node> node, const char* ipaddr, const char* netmask)
-{
-  Ptr<SimpleNetDevice> dev = CreateObject<SimpleNetDevice> ();
-  dev->SetAddress (Mac48Address::ConvertFrom (Mac48Address::Allocate ()));
-  node->AddDevice (dev);
-  Ptr<Ipv4> ipv4 = node->GetObject<Ipv4> ();
-  uint32_t ndid = ipv4->AddInterface (dev);
-  Ipv4InterfaceAddress ipv4Addr = Ipv4InterfaceAddress (Ipv4Address (ipaddr), Ipv4Mask (netmask));
-  ipv4->AddAddress (ndid, ipv4Addr);
-  ipv4->SetUp (ndid);
-  return dev;
-}
-
-void
-MpTcpTestCase::SetupDefaultSim (void)
-{
-  const char* netmask = "255.255.255.0";
-  const char* ipaddr0 = "192.168.1.1";
-  const char* ipaddr1 = "192.168.1.2";
-  Ptr<Node> node0 = CreateInternetNode ();
-  Ptr<Node> node1 = CreateInternetNode ();
-  Ptr<SimpleNetDevice> dev0 = AddSimpleNetDevice (node0, ipaddr0, netmask);
-  Ptr<SimpleNetDevice> dev1 = AddSimpleNetDevice (node1, ipaddr1, netmask);
-
-  Ptr<SimpleChannel> channel = CreateObject<SimpleChannel> ();
-  dev0->SetChannel (channel);
-  dev1->SetChannel (channel);
-
-  Ptr<SocketFactory> sockFactory0 = node0->GetObject<TcpSocketFactory> ();
-  Ptr<SocketFactory> sockFactory1 = node1->GetObject<TcpSocketFactory> ();
-
-  Ptr<Socket> server = sockFactory0->CreateSocket ();
-  Ptr<Socket> source = sockFactory1->CreateSocket ();
-
-  uint16_t port = 50000;
-  InetSocketAddress serverlocaladdr (Ipv4Address::GetAny (), port);
-  InetSocketAddress serverremoteaddr (Ipv4Address (ipaddr0), port);
-
-  server->Bind (serverlocaladdr);
-  server->Listen ();
-  server->SetAcceptCallback (MakeNullCallback<bool, Ptr< Socket >, const Address &> (),
-                             MakeCallback (&MpTcpTestCase::ServerHandleConnectionCreated,this));
-
-  source->SetRecvCallback (MakeCallback (&MpTcpTestCase::SourceHandleRecv, this));
-  source->SetSendCallback (MakeCallback (&MpTcpTestCase::SourceHandleSend, this));
-
-  source->Connect (serverremoteaddr);
-}
-
-void
-MpTcpTestCase::SetupDefaultSim6 (void)
-{
-  Ipv6Prefix prefix = Ipv6Prefix(64);
-  Ipv6Address ipaddr0 = Ipv6Address("2001:0100:f00d:cafe::1");
-  Ipv6Address ipaddr1 = Ipv6Address("2001:0100:f00d:cafe::2");
-  Ptr<Node> node0 = CreateInternetNode6 ();
-  Ptr<Node> node1 = CreateInternetNode6 ();
-  Ptr<SimpleNetDevice> dev0 = AddSimpleNetDevice6 (node0, ipaddr0, prefix);
-  Ptr<SimpleNetDevice> dev1 = AddSimpleNetDevice6 (node1, ipaddr1, prefix);
-
-  Ptr<SimpleChannel> channel = CreateObject<SimpleChannel> ();
-  dev0->SetChannel (channel);
-  dev1->SetChannel (channel);
-
-  Ptr<SocketFactory> sockFactory0 = node0->GetObject<TcpSocketFactory> ();
-  Ptr<SocketFactory> sockFactory1 = node1->GetObject<TcpSocketFactory> ();
-
-  Ptr<Socket> server = sockFactory0->CreateSocket ();
-  Ptr<Socket> source = sockFactory1->CreateSocket ();
-
-  uint16_t port = 50000;
-  Inet6SocketAddress serverlocaladdr (Ipv6Address::GetAny (), port);
-  Inet6SocketAddress serverremoteaddr (ipaddr0, port);
-
-  server->Bind (serverlocaladdr);
-  server->Listen ();
-  server->SetAcceptCallback (MakeNullCallback<bool, Ptr< Socket >, const Address &> (),
-                             MakeCallback (&MpTcpTestCase::ServerHandleConnectionCreated,this));
-
-  source->SetRecvCallback (MakeCallback (&MpTcpTestCase::SourceHandleRecv, this));
-  source->SetSendCallback (MakeCallback (&MpTcpTestCase::SourceHandleSend, this));
-
-  source->Connect (serverremoteaddr);
-}
-
-Ptr<Node>
-MpTcpTestCase::CreateInternetNode6 ()
-{
-  Ptr<Node> node = CreateObject<Node> ();
-  //IPV6
-  Ptr<Ipv6L3Protocol> ipv6 = CreateObject<Ipv6L3Protocol> ();
-  //Routing for Ipv6
-  Ptr<Ipv6ListRouting> ipv6Routing = CreateObject<Ipv6ListRouting> ();
-  ipv6->SetRoutingProtocol (ipv6Routing);
-  Ptr<Ipv6StaticRouting> ipv6staticRouting = CreateObject<Ipv6StaticRouting> ();
-  ipv6Routing->AddRoutingProtocol (ipv6staticRouting, 0);
-  node->AggregateObject (ipv6);
-  //ICMP
-  Ptr<Icmpv6L4Protocol> icmp = CreateObject<Icmpv6L4Protocol> ();
-  node->AggregateObject (icmp);
-  //Ipv6 Extensions
-  ipv6->RegisterExtensions ();
-  ipv6->RegisterOptions ();
-  //UDP
-  Ptr<UdpL4Protocol> udp = CreateObject<UdpL4Protocol> ();
-  node->AggregateObject (udp);
-  //TCP
-  Ptr<TcpL4Protocol> tcp = CreateObject<TcpL4Protocol> ();
-  node->AggregateObject (tcp);
-  return node;
-}
-
-Ptr<SimpleNetDevice>
-MpTcpTestCase::AddSimpleNetDevice6 (Ptr<Node> node, Ipv6Address ipaddr, Ipv6Prefix prefix)
-{
-  Ptr<SimpleNetDevice> dev = CreateObject<SimpleNetDevice> ();
-  dev->SetAddress (Mac48Address::ConvertFrom (Mac48Address::Allocate ()));
-  node->AddDevice (dev);
-  Ptr<Ipv6> ipv6 = node->GetObject<Ipv6> ();
-  uint32_t ndid = ipv6->AddInterface (dev);
-  Ipv6InterfaceAddress ipv6Addr = Ipv6InterfaceAddress (ipaddr, prefix);
-  ipv6->AddAddress (ndid, ipv6Addr);
-  ipv6->SetUp (ndid);
-  return dev;
-}
-#endif
-
-
 
