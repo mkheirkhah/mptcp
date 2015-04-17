510db525f945675a7a2476174e404d70f03c107a
Morteza Kheirkhah <m.kheirkhah@sussex.ac.uk>
2015-04-17 16:55:00 +0100

    Updates Licenses

--- src/internet/model/mp-tcp-subflow.cc
+++ src/internet/model/mp-tcp-subflow.cc
@@ -1,8 +1,21 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * MultiPath-TCP (MPTCP) implementation.
- * Programmed by Morteza Kheirkhah from University of Sussex.
- * Some codes here are modeled from ns3::TCPNewReno implementation.
- * Email: m.kheirkhah@sussex.ac.uk
+ * Copyright (c) <2013-2015> University Of Sussex
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Morteza Kheirkhah <m.kheirkhah@sussex.ac.uk>
  */
 #include <iostream>
 #include "ns3/mp-tcp-typedefs.h"
--- src/internet/model/tcp-l4-protocol.cc
+++ src/internet/model/tcp-l4-protocol.cc
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author: Raj Bhattacharjea <raj.b@gatech.edu>
- * Modified: Morteza Kheirkhah <m.kheirkhah@sussex.ac.uk>, University of Sussex, UK.
+ * Modified: Morteza Kheirkhah <m.kheirkhah@sussex.ac.uk>
  */
 
 #include "ns3/assert.h"
