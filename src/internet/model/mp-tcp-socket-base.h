f8ae961cd67658c3c31c299a8d400b01cbffb628
Morteza Kheirkhah <m.kheirkhah@sussex.ac.uk>
2015-04-18 21:51:41 +0100

    LC

--- src/internet/model/mp-tcp-socket-base.h
+++ src/internet/model/mp-tcp-socket-base.h
@@ -1,9 +1,23 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * MultiPath-TCP (MPTCP) implementation.
- * Programmed by Morteza Kheirkhah from University of Sussex.
- * Some codes here are modeled from ns3::TCPNewReno implementation.
- * Email: m.kheirkhah@sussex.ac.uk
+ * Copyright (c) 2014 Morteza Kheirkhah
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Morteza Kheirkhah <m.kheirkhah@sussex.ac.uk>
  */
+
 #ifndef MP_TCP_SOCKET_BASE_H
 #define MP_TCP_SOCKET_BASE_H
 
